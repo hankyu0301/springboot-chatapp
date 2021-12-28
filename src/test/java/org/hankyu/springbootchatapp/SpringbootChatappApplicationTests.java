a32a20ffc38cb7f59e3783a72ec2922d8cf24759
hankyujang <finebears@naver.com>
2021-12-28 15:03:03 +0900

    Initial commit

---
+++ src/test/java/org/hankyu/springbootchatapp/SpringbootChatappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hankyu.springbootchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootChatappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
